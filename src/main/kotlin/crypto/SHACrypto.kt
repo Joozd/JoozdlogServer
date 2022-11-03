05f68c33aadae8a692aa391a76e7cb5817f5325f
joozd <euroshopper@gmail.com>
2022-11-03 09:28:52 +0100

    Removed unused functions

--- src/main/kotlin/crypto/SHACrypto.kt
+++ src/main/kotlin/crypto/SHACrypto.kt
@@ -1,22 +1,11 @@
 package crypto
 
 import java.security.MessageDigest
+import java.security.SecureRandom
 
 object SHACrypto {
-    private val EXTRA_SALT = "JoozdLog".toByteArray()
-
-    fun hash(itemToHash: ByteArray): ByteArray =
-        MessageDigest.getInstance("SHA-256").digest(itemToHash)
-
-    fun hashWithSalt(salt: ByteArray, item: ByteArray): ByteArray =
-        MessageDigest.getInstance("SHA-256").digest(salt + item)
-
-    fun hashWithSalt(salt: ByteArray, item: String): ByteArray =
+    fun hash(item: String, salt: ByteArray): ByteArray =
         MessageDigest.getInstance("SHA-256").digest(salt + item.toByteArray(Charsets.UTF_8))
 
-    /**
-     * Adds a bit of extra salt so rainbow table of known hash of password with username as salt won't work
-     */
-    fun hashWithExtraSalt(salt: String, item: ByteArray): ByteArray =
-        MessageDigest.getInstance("SHA-256").digest(salt.toByteArray() + EXTRA_SALT + item)
+    fun generateRandomSalt() = ByteArray(16).apply{ SecureRandom().nextBytes(this) }
 }
