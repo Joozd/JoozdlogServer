080cdd9bb36e7e51747539f3762da72642b026d0
joozd <euroshopper@gmail.com>
2022-11-03 09:32:39 +0100

    sendError now takes a JoozdlogCommsResponse instead of a string.

--- src/main/kotlin/extensions/IOWorkerExt.kt
+++ src/main/kotlin/extensions/IOWorkerExt.kt
@@ -1,7 +1,8 @@
 package extensions
 
 import nl.joozd.comms.IOWorker
+import nl.joozd.joozdlogcommon.comms.JoozdlogCommsResponses
 
-fun IOWorker.sendError(error: String){
-    write(error)
+fun IOWorker.sendError(error: JoozdlogCommsResponses){
+    write(error.keyword)
 }
