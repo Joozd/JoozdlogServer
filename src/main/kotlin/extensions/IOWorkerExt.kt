0d142e8a51b181b4a97eff79b00be2f2657c0be9
joozd <euroshopper@gmail.com>
2022-11-07 14:43:30 +0100

    sending an error to clients now ends up in verbose log.

--- src/main/kotlin/extensions/IOWorkerExt.kt
+++ src/main/kotlin/extensions/IOWorkerExt.kt
@@ -2,7 +2,10 @@ package extensions
 
 import nl.joozd.comms.IOWorker
 import nl.joozd.joozdlogcommon.comms.JoozdlogCommsResponses
+import utils.Logger
+
 
 fun IOWorker.sendError(error: JoozdlogCommsResponses){
+    Logger.singleton.v("Error: ${error.keyword} sent to ${this.otherAddress}")
     write(error.keyword)
 }
